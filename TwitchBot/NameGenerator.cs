57dc39e1a30ac6ef4b139603bc589db4d84d5eaa
Aron Davis <arontd@gmail.com>
2015-10-06 23:57:25 -0400

    Initial Commit
    Still heavily a work in progress.  Refactor needs to happen before
    moving on.

---
+++ TwitchBot/NameGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchBot
+{
+    class NameGenerator
+    {
+        private Random random = new Random();
+        private int minSyllables = 2;
+        private int maxSyllables = 4;
+
+        private string[] consonants = new string[] { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "qu", "r", "s", "t", "v", "w", "x", "y", "z" };
+        private string[] vowels = new string[] { "a", "e", "i", "o", "u" };
+
+        public NameGenerator()
+        {
+
+        }
+
+        public string GetName()
+        {
+            int syllables = random.Next(minSyllables, maxSyllables+1);
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < syllables; i++)
+            {
+                sb.Append(consonants[random.Next(0, consonants.Length)]);
+                sb.Append(vowels[random.Next(0, vowels.Length)]);
+            }
+            sb[0] = sb[0].ToString().ToUpper()[0];
+            return sb.ToString();
+        }
+    }
+}
---
+++ TwitchBot/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Configuration;
+using System.Text.RegularExpressions;
+
+namespace TwitchBot
+{
+    class Program
+    {
+        private static IrcClient irc;
+        static void Main(string[] args)
+        {
+            string password = ConfigurationManager.AppSettings["oauth"];
+
+            //password from www.twitchapps.com/tmi
+            //include the "oauth:" portion
+            irc = new IrcClient("irc.twitch.tv", 6667, "mrsheila", password);
+
+            //join channel
+            irc.JoinRoom("voxdavis");
+
+            //TODO: gross, change!
+            while (true)
+            {
+                string message = irc.readMessage();
+
+                Console.WriteLine(message);
+
+                if (message.IndexOf("!") >= 0)
+                {
+                    handleChatMessage(message);
+                }
+            }
+        }
+
+        private static void handleChatMessage(string message)
+        {
+            string username = message.Substring(1, message.IndexOf("!") - 1);
+            message = message.Substring(message.IndexOf(":") + 1);
+            message = message.Substring(message.IndexOf(":") + 1);
+
+            if (message[0] == '!')
+            {
+                handleCommand(username, message);
+            }
+
+        }
+
+        private static void handleCommand(string username, string message)
+        {
+            if (message == "!hype")
+            {
+                irc.sentChatMessage("@" + username + " HYPE HYPE HYPE!!!! at " + DateTime.Now.ToLongTimeString());
+            }
+            else if (message == "!name")
+            {
+                NameGenerator ng = new NameGenerator();
+                irc.sentChatMessage(ng.GetName());
+            }
+            else if(message.StartsWith("!name"))
+            {
+                Regex r = new Regex(@"!name @[\w_\-]+");
+
+                if (r.IsMatch(message))
+                {
+                    NameGenerator ng = new NameGenerator();
+                    string u = message.Substring(7);
+                    irc.sentChatMessage(u + "'s new name is " + ng.GetName());
+                }
+            }
+        }
+
+    }
+}
